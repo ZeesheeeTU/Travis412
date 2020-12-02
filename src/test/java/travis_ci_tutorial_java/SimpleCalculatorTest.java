3f75df3c65601ba6f94164e7306683577e4be821
zhaide1 <zhaide1@students.towson.edu>
2020-12-01 19:33:34 -0500

    error test

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -10,5 +10,9 @@ public class SimpleCalculatorTest {
 	public void testAdd() {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
+		assertEquals(calc.subtract(1, 1), 0);
+		assertEquals(calc.multiply(1, 1), 1);
+		assertEquals(calc.divide(2, 1), 2);
+		assertEquals(calc.subtract(3, 5), 2);
 	}
 }
